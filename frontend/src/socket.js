f93b38055e638a8d8e0a6e1fe518d8286875baae
Ayush Rohan <ayushrohan5@gmail.com>
2025-09-28 21:44:52 +0530

    first commit

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client'
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'https://mern-quizwalsis-steel.vercel.app'
-export const createSocket = () => io(SOCKET_URL, { transports: ['websocket'] })
+export const createSocket = () => io(SOCKET_URL, { transports: ['polling'] })
