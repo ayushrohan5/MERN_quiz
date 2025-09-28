522114c11481b54c38dc41745a70bc2bdf5b84aa
Ayush Rohan <ayushrohan5@gmail.com>
2025-09-28 20:59:43 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: { origin: '*' } });
+const io = new Server(httpServer, { cors: { origin: '*' }, transports: ['polling', 'websocket'] });
 
 app.use(cors());
 app.use(express.json());
